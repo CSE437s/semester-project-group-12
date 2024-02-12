69d8216904fbd957ffeaa2caae45f6d1ca06f4bd
Junxian Zhang <junxianazhang@gmail.com>
2024-02-11 22:14:01 -0600

    Search Bar UI

--- Semester-Project/App.js
+++ Semester-Project/App.js
@@ -1,20 +1,38 @@
+import { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, FlatList } from 'react-native';
+import { SearchBar } from 'react-native-elements';
 
 export default function App() {
+  const [search, setSearch] = useState('');
+
+  const updateSearch = (text) => {
+    setSearch(text);
+    
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
+      <SearchBar
+        placeholder="Type Here..."
+        onChangeText={updateSearch}
+        containerStyle={{ width: '100%' }}
+        value={search}
+      />
+     
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
+    
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    // flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    flexDirection: 'row',
   },
+ 
+  
 });
