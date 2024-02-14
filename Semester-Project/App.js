8fac21809ef3558851ed0dfa197161f30263f9cd
Junxian Zhang <junxianazhang@gmail.com>
2024-02-13 19:13:59 -0600

    commit

--- Semester-Project/App.js
+++ Semester-Project/App.js
@@ -1,11 +1,16 @@
 import { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, SafeAreaView, FlatList, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, FlatList, TouchableOpacity, TextInput, Button } from 'react-native';
 import { SearchBar } from 'react-native-elements';
+import { createUserWithEmailAndPassword, connectAuthEmulator } from "firebase/auth"
+import { auth } from './firebaseConfig';
 
 export default function App() {
   const [search, setSearch] = useState('');
   const [suggestions, setSuggestions] = useState([]);
+  const [email, onChangeEmail] = useState('');
+  const [password, onChangePassword] = useState('');
+
   const suggestionData = require('./STLNeighborhoods.json').neighborhoods;
 
   const updateSearch = (text) => {
@@ -31,9 +36,28 @@ export default function App() {
 
   );
 
+  const signUp = async (email, password) => {
+   console.log("email " + email)
+   console.log("password " + password)
+    console.log("pressed")
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed up 
+        const user = userCredential.user;
+        console.log(user)
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log(errorCode)
+        console.log(errorMessage)
+      });
+
+  }
+
   return (
     <SafeAreaView>
-      <View style={styles.container}>
+      {/* <View style={styles.container}>
         <SearchBar
           placeholder="Type your neighborhood here..."
           onChangeText={updateSearch}
@@ -51,7 +75,24 @@ export default function App() {
         />
       </View>
 
-      <StatusBar style="auto" />
+      <StatusBar style="auto" /> */}
+      <Text>
+
+      </Text>
+      <TextInput
+        style={styles.input}
+        onChangeText={onChangeEmail}
+        value={email}
+      />
+      <TextInput
+        style={styles.input}
+        onChangeText={onChangePassword}
+        value={password}
+      />
+      <Button
+        title="Sign Up"
+        onPress={signUp}
+      />
     </SafeAreaView>
 
   );
@@ -68,7 +109,13 @@ const styles = StyleSheet.create({
     padding: 15,
     borderBottomWidth: 1,
     borderBottomColor: '#ccc',
-  }
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
 
 
 });
--- Semester-Project/firebaseConfig.js
+++ Semester-Project/firebaseConfig.js
@@ -1,6 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth"// Import the functions you need from the SDKs you need
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -12,10 +14,11 @@ const firebaseConfig = {
   projectId: "safecities-773a7",
   storageBucket: "safecities-773a7.appspot.com",
   messagingSenderId: "558662647206",
-  appId: "1:558662647206:web:b9d06b46d10940dfc0e367",
-  measurementId: "G-XKTX630FJE"
+  appId: "1:558662647206:web:12abdfe26185fcd2c0e367",
+  measurementId: "G-JXT30DNE87"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-// const analytics = getAnalytics(app);
+const analytics = getAnalytics(app);
+export const auth  = getAuth(app)
